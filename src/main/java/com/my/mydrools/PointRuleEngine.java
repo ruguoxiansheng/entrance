162e814aa7ad278e79ab39af58a41f69694216c4
ruguoxiansheng <by_wwj@163.com>
2017-11-22 11:46:04 +0800

    规则引擎
    规则引擎运行的简单例子。

---
+++ src/main/java/com/my/mydrools/PointRuleEngine.java
@@ -0,0 +1,21 @@
+package com.my.mydrools;
+
+public interface PointRuleEngine {
+	/**
+	 * 初始化规则引擎
+	 */
+	public void initEngine();
+
+	/**
+	 * 刷新规则引擎中的规则
+	 */
+	public void refreshEnginRule();
+
+	/**
+	 * 执行规则引擎
+	 * 
+	 * @param pointDomain
+	 *            积分Fact
+	 */
+	public void executeRuleEngine(final PointDomain pointDomain);
+}
